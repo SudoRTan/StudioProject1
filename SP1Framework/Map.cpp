0b4e168a626e3d3f0e097241be5e26787ba87cb8
SudoRTan <rayntanyz@gmail.com>
2020-08-17 11:59:18 +0800

    Added simple Map class to store map items as well as simple printing of the map

--- SP1Framework/Map.cpp
+++ SP1Framework/Map.cpp
@@ -1,1 +1,27 @@
 #include "Map.h"
+
+Map::Map() {
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 80; j++) {
+			mapArray[i][j] = ' ';
+		}
+	}
+	for (int i = 0; i < 80; i++) {
+		mapArray[15][i] = (char)FLOOR;
+	}
+}
+
+Map::~Map() {
+
+}
+
+void Map::renderMap(Console& console) {
+	const int yOffset = 9;
+
+	for (int i = 0; i < 16; i++) {
+		for (int j = 0; j < 80; j++) {
+			console.writeToBuffer(j, i + yOffset, mapArray[i][j],BG_CYAN+FG_LIGHTMAGENTA);
+		}
+	}
+}
+
--- SP1Framework/Map.h
+++ SP1Framework/Map.h
@@ -1,5 +1,25 @@
+﻿#include "Framework/console.h"
+#include "colors.h"
 #pragma once
+
+enum MapElements {
+	EMPTY = ' ',
+	FLOOR = 178,
+};
+
 class Map
 {
+private:
+	char mapArray[16][80];
+
+public:
+	//Constructor/Destructor
+	Map();
+	~Map();
+
+	//Renders map to console
+	void renderMap(Console& console);
+
+
 };
 
