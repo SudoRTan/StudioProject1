d3605bce01bb9518666aa78adb2af8584438363c
yeokhao <69415162+yeokhao@users.noreply.github.com>
2020-08-17 11:51:24 +0800

    Added constructors, destructors and class functions for Collectible class and Enemy class. There are currently errors as this version is not linked to Position class and unable to create pos Object or use Entity class functions.

--- SP1Framework/Collectible.h
+++ SP1Framework/Collectible.h
@@ -1,5 +1,13 @@
 #pragma once
 class Collectible
 {
+private:
+	bool isCollected;
+	int CollectibleCount = 0;
+
+public:
+	Collectible();
+	~Collectible();
+	void PlayerCollect();
 };
 
--- SP1Framework/Enemy.cpp
+++ SP1Framework/Enemy.cpp
@@ -1,1 +1,30 @@
 #include "Enemy.h"
+
+#include <random>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+Enemy::Enemy()
+{
+
+}
+
+Enemy::~Enemy()
+{
+
+}
+
+void Enemy::Movement()
+{
+	for (int left = 0; left < 3; left++) // 3 is a temp number, change later or use different code for range of enemy movement
+	{
+		pos.moveLeft(); // call entity function to move left on map
+	}
+	for (int right = 0; right < 3; right++)
+	{
+		pos.moveRight(); // call entity function to move right on map
+	}
+
+	//print out map with updated x and y values of enemy (in main or somewhere else idk)
+}
