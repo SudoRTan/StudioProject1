3268a973bc076dbdcbf96e1a1bb45f43317dc79a
MUHAMMAD FARID B SUHAIMI <202744A@mymail.nyp.edu.sg>
2020-08-17 12:22:25 +0800

    Added hp bar

--- SP1Framework/UI.cpp
+++ SP1Framework/UI.cpp
@@ -1,1 +1,12 @@
 #include "UI.h"
+
+cout << Health + '/' + Maxhealth;
+float ShowHealth = float(this->getHealth()) / (float(this->getMaxHealth() / 10));
+float HiddenHealth = 10 - ShowHealth
+{
+	for (int i = 0, i < ShowHealth, i++)
+		cout << '[0]';
+	for (int i = 0, i < HideHealth, i++)
+		cout << '[]';
+
+};
--- SP1Framework/UI.h
+++ SP1Framework/UI.h
@@ -1,5 +1,20 @@
 #pragma once
+using namespace std;
+
 class UI
 {
+ public:
+		int Health;
+		int Maxhealth = 100;
+		unsigned int getHealth;
+	
+    int UI::getMaxHealth;
+	int UI::setMaxHealth;
+
+	
+
+
+
+	
 };
 
