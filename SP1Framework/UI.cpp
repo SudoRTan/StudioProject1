6a08ad68d451f2210318f8c8fdd4065733ec24c1
MUHAMMAD FARID B SUHAIMI <202744A@mymail.nyp.edu.sg>
2020-08-18 15:55:59 +0800

    Added weapons class

--- SP1Framework/UI.cpp
+++ SP1Framework/UI.cpp
@@ -37,23 +37,9 @@ void UI::HealthBar(Console& console,Player& player)
 		for (int i = 0; i < int(ShowHealth); i++);
 		console.writeToBuffer(6,0,(char)48,'[0]');
 		for (int i = 0; i < int(HiddenHealth); i++);
-		console.writeToBuffer(6,0,(char)48,'[]');
+		console.writeToBuffer(6,0,(char)48,'[ ]');
 
 	};
 
 };
 
-void Weapons::AmmoCounter(Console& console)
-{
-	console.writeToBuffer(0,1,(char)48, 'Ammo');
-	float ShowAmmo = Weapons::getAmmo() / 10;
-	float HiddenAmmo = 10 - ShowAmmo;
-	{
-		for (int i = 0; i < int(ShowAmmo); i++);
-		console.writeToBuffer(6,1,char(48),'[0]');
-		for (int i = 0; i < int(HiddenAmmo); i++);
-		console.writeToBuffer(6,1,char(48), '[]');
-
-	}
-	
-}
