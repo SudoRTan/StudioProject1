be7a8a5f296f2b25d636f828f9555aa7983f0240
yeokhao <69415162+yeokhao@users.noreply.github.com>
2020-08-25 15:07:56 +0800

    Added classes for different enemy types. Also edited levels slightly.

---
+++ SP1Framework/EnemyType1.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Enemy.h"
+class EnemyType1
+	: public Enemy
+{
+private:
+public:
+	EnemyType1();
+	~EnemyType1();
+	int update(Map& map, double g_dElapsedTime, Player& player);
+
+};
+
