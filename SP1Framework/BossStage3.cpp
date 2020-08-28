8526ed8207f3f48d0e7f4836709ee91d885eb199
ZACHARY TAN <203202U@mymail.nyp.edu.sg>
2020-08-28 17:28:37 +0800

    exception thrown at cleanUp() constructor only for 3-4

--- SP1Framework/BossStage3.cpp
+++ SP1Framework/BossStage3.cpp
@@ -4,10 +4,27 @@ BossStage3::BossStage3(Player* player): Stage(player)
 {
 	numberOfVirus = 1;
 	virus = new Enemy* [numberOfVirus];
+	maxNumberOfVirus = 1;
 	virus[0] = new BossStage3Virus;
 }
 
 BossStage3::~BossStage3()
 {
 
+}
+
+void BossStage3::update(SKeyEvent KeyEvent[K_COUNT], double g_dElapsedTime, int& gameState)
+{
+	entityManager.update(*map, KeyEvent, g_dElapsedTime, gameState, virus, numberOfVirus);
+	
+	for (int i = 0; i < maxNumberOfVirus; i++)
+	{
+		if (virus != nullptr)
+			virus[i]->update(*map, g_dElapsedTime, *player);
+		else
+			numberOfVirus--;
+	}
+
+	if (numberOfVirus == 0)
+		gameState = LEVEL_COMPLETE_MENU;
 }
