78385faf874b6efbaafdf10189659e953df8aa62
SudoRTan <rayntanyz@gmail.com>
2020-08-29 16:48:51 +0800

    Updated BossStage3 comments

--- SP1Framework/BossStage3.cpp
+++ SP1Framework/BossStage3.cpp
@@ -2,13 +2,14 @@
 
 BossStage3::BossStage3(Player* player): Stage(player)
 {
+	// Spawns a new Virus boss
 	maxNumberOfVirus = 1;
 	virus = new Enemy* [maxNumberOfVirus];
 	virus[0] = new BossStage3Virus;
 }
 
 BossStage3::~BossStage3()
-{
+{ // Clears the virus pointer array
 	if (virus[0] != nullptr) {
 		delete virus[0];
 		virus[0] = nullptr;
@@ -17,8 +18,10 @@ BossStage3::~BossStage3()
 
 void BossStage3::update(SKeyEvent KeyEvent[K_COUNT], double g_dElapsedTime, int& gameState)
 {
+	// Update all the entities inside the stage
 	entityManager.update(*map, KeyEvent, g_dElapsedTime, gameState, virus, maxNumberOfVirus);
 	
+	// End level if virus is dead.
 	if (virus[0] == nullptr) {
 		gameState = LEVEL_COMPLETE_MENU;
 	}
