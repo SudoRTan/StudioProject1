07f9138e8e81a210e93fa147cf10ef8aa1ffc04f
SudoRTan <rayntanyz@gmail.com>
2020-08-29 21:37:21 +0800

    Started work on exit button, no annimations currently

--- SP1Framework/StartMenu.cpp
+++ SP1Framework/StartMenu.cpp
@@ -2,7 +2,7 @@
 
 StartMenu::StartMenu() {
 	currentSelection = 1;
-	totalSelections = 1;
+	totalSelections = 2;
 }
 
 StartMenu::~StartMenu() {
@@ -10,10 +10,37 @@ StartMenu::~StartMenu() {
 }
 
 void StartMenu::update(int& gameState, SKeyEvent KeyEvent[K_COUNT], int& currStage, int& currLevel, int unlockedStage, int unlockedLevel) {
-	if (KeyEvent[K_SPACE].keyOnce) {
-		gameState = LEVEL_SELECT;
-		PlaySound(TEXT("./Sounds/menuConfirm.wav"), NULL, SND_FILENAME | SND_ASYNC);
+	if (KeyEvent[K_UP].keyOnce || KeyEvent[K_LEFT].keyOnce) {
+		decreaseSelection();
+		PlaySound(TEXT("./Sounds/menuSelect.wav"), NULL, SND_FILENAME | SND_ASYNC);
 	}
+
+	else if (KeyEvent[K_DOWN].keyOnce || KeyEvent[K_RIGHT].keyOnce) {
+		increaseSelection();
+		PlaySound(TEXT("./Sounds/menuSelect.wav"), NULL, SND_FILENAME | SND_ASYNC);
+	}
+
+	else if (KeyEvent[K_SPACE].keyOnce) {
+		if (currentSelection == 1) {
+			gameState = LEVEL_SELECT;
+			PlaySound(TEXT("./Sounds/menuConfirm.wav"), NULL, SND_FILENAME | SND_ASYNC);
+		}
+		else {
+			currentSelection = 1;
+			gameState = EXIT_GAME;
+			PlaySound(TEXT("./Sounds/menuconfirm.wav"), NULL, SND_FILENAME | SND_ASYNC);
+		}
+	}
+	else if (KeyEvent[K_ESCAPE].keyOnce) {
+		gameState = EXIT_GAME;
+		PlaySound(TEXT("./Sounds/menuEscape.wav"), NULL, SND_FILENAME | SND_ASYNC);
+			
+	}
+	
+//	if (KeyEvent[K_SPACE].keyOnce) {
+		//gameState = LEVEL_SELECT;
+	//	PlaySound(TEXT("./Sounds/menuConfirm.wav"), NULL, SND_FILENAME | SND_ASYNC);
+//	}
 }
 
 void StartMenu::render(Console& console) {
