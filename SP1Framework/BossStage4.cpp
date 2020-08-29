83949e727b2ec2165677f843530b26b20c827c48
ZACHARY TAN <203202U@mymail.nyp.edu.sg>
2020-08-29 17:07:19 +0800

    boss stage 4 updated till player dies, require testing for code and fix problem of overriding update() function

--- SP1Framework/BossStage4.cpp
+++ SP1Framework/BossStage4.cpp
@@ -1,1 +1,28 @@
 #include "BossStage4.h"
+
+BossStage4::BossStage4(Player* player) : Stage(player)
+{
+	enemies = new Enemy* [2];
+	enemies[0] = new BossStage4People;
+	enemies[1] = new BossStage4People;
+}
+
+BossStage4::~BossStage4()
+{
+	for (int i = 0; i < 2; i++)
+	{
+		delete enemies[i];
+		enemies[i] = nullptr;
+	}
+}
+
+void BossStage4::update(SKeyEvent KeyEvent[K_COUNT], double g_dElapsedTime, int& gameState)
+{
+	while (player->getHealth() != 0)
+	{
+		for (int i = 0; i < 2; i++)
+		{
+			enemies[i]->update(*map, g_dElapsedTime, *player);
+		}
+	}
+}
